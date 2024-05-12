01323e9032fd87cc5c63bd1338de9f6af2e4b6ca
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-05-12 21:33:24 +0530

    first-change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,19 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+    B b = new B();
+    //b.meth();
+    System.out.println(b.meth());
+  }
+}
+class A{
+    String meth(){
+        return "Danish";
+    }
+}
+class B extends A{
+  String meth(){
+    return "Method is overridden in Extended class B";
+  }
 }
