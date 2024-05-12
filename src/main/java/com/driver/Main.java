04904bcda5490ce9ed4f5717ee7d746f77d52da4
alidanish935 <114473927+alidanish935@users.noreply.github.com>
2024-05-12 21:34:00 +0530

    sec-change

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,8 +3,8 @@ package com.driver;
 public class Main {
   public static void main(String[] args) {
     B b = new B();
-    //b.meth();
-    System.out.println(b.meth());
+    b.meth();
+    // System.out.println(b.meth());
   }
 }
 class A{
